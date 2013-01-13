37026aa8604e2918f46b5d39c3d6a1c1e7fb7500
ftoulemon <florentin.toulemon@gmail.com>
2013-01-13 13:32:10 +0100

    add switch on/off

--- openVPN_indicator.py
+++ openVPN_indicator.py
@@ -13,11 +13,18 @@ class OpenVPNInd:
 				appindicator.CATEGORY_APPLICATION_STATUS)
 		self.ind.set_status(appindicator.STATUS_ACTIVE)
 		self.ind.set_attention_icon("/home/ftoulemon/dev/openvpn_indicator/lock_ok.png")
+
 		self.menu_setup()
 		self.ind.set_menu(self.menu)
 
 	def menu_setup(self):
 		self.menu = gtk.Menu()
+
+		self.on_off_item = gtk.MenuItem("vpn on/off")
+		self.on_off_item.connect("activate", self.switch)
+		self.on_off_item.show()
+		self.menu.append(self.on_off_item)
+
 		self.quit_item = gtk.MenuItem("Quit")
 		self.quit_item.connect("activate", self.quit)
 		self.quit_item.show()
@@ -25,14 +32,21 @@ class OpenVPNInd:
 		
 	def main(self):
 		self.check_vpn()
-		gtk.timeout_add(PING_FREQUENCY * 100, self.check_vpn)
+		gtk.timeout_add(PING_FREQUENCY * 1000, self.check_vpn)
 		gtk.main()
+
+	def switch(self, widget):
+		if self.ind.get_status == appindicator.STATUS_ATTENTION:
+			print '1'
+			self.ind.set_status(appindicator.STATUS_ACTIVE)
+		if self.ind.get_status == appindicator.STATUS_ACTIVE:
+			print '2'
+			self.ind.set_status(appindicator.STATUS_ATTENTION)
 
 	def quit(self, widget):
 		sys.exit(0)
 
 	def check_vpn(self):
-		self.ind.set_status(appindicator.STATUS_ATTENTION)
 		return True
 
 if __name__== "__main__":
